b4731b36b9189c5e0a378cda0905d08505b5dbaf
Jose Bayon <jose.bayon1411@gmail.com>
2025-05-05 18:01:19 +0200

    Agregando app_theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// Archivo de configuracion de los temas
+
+// Lista de colores para el tema
+const colorList = <Color>[
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.lime,
+  Colors.indigo,
+  Colors.amber,
+];
+
+// Clase que define el tema de la aplicacion
+class AppTheme {
+
+  // Indice del color seleccionado
+  final int selectedColorIndex;
+
+  // Constructor de la clase
+  AppTheme({this.selectedColorIndex = 0});
+
+  // Metodo que devuelve el tema de la aplicacion
+  ThemeData themeData() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColorIndex],
+    brightness: Brightness.light,
+  );
+}
