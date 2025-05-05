7eed1c7c392ab3726089a9409eff42313623a188
Jose Bayon <jose.bayon1411@gmail.com>
2025-05-05 17:57:32 +0200

    Primer commit, Proyecto Dog Share, creando la estructura del proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# dog_share
+
+A new Flutter project.
---
+++ lib/presentation/screens_export.dart
@@ -0,0 +1,7 @@
+
+
+
+// Archivo de barril que contiene las pantallas de la aplicación
+// Se exportan las pantallas para que se puedan utilizar en otras partes de la aplicación
+export 'package:dog_share/presentation/screens/login_screen.dart';
+export 'package:dog_share/presentation/screens/register_screen.dart';
